364bcf06f64f7aae9ab48df193c18df2f2794d48
ashwini <ashwini@dabkick.com>
2019-01-24 11:11:37 +0530

    changed alert dialogs theme from red all caps to default android alert box colors and sentence case

--- app/src/main/java/dabkick/com/basicsampleapp/ChatRoomFragment.java
+++ app/src/main/java/dabkick/com/basicsampleapp/ChatRoomFragment.java
@@ -466,7 +466,7 @@ public class ChatRoomFragment extends Fragment {
 
 
     public void showAlertDialogWhileExiting() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(BaseActivity.mCurrentActivity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(BaseActivity.mCurrentActivity, R.style.ExitChatRoomDialogTheme);
         builder.setMessage("Would you like to")
                 .setPositiveButton("Stay Subscribed - You will still listen to messages", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
